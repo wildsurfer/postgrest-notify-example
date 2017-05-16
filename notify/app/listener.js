9fdc1c0f6bf49b23100d8689f7c86023e722d2c6
vanya andreychuk <vandreychyk@gmail.com>
2017-05-16 16:59:24 +0300

    Feat: Add Notify

---
+++ notify/app/listener.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+let amqp = require('amqplib/callback_api');
+let PS = require('pg-pubsub');
+
+let rabbitConnectionString = process.env.RABBIT_CONNECTION_STRING || 'amqp://guest:guest@rabbitmq';
+
+let cn = {
+    host: process.env.POSTGRES_HOST || "postgresql", // server name or IP address;
+    port: 5432,
+    database: process.env.POSTGRES_DATABASE || "test",
+    user: process.env.POSTGRES_USER || "postgres",
+    password: process.env.POSTGRES_PASSWORD || "DGH5k39xchgh3Po",
+    getConnectionString: function () {
+        return 'postgres://' + this.user + ':' + this.password + '@' + this.host + '/' + this.database;
+    }
+};
+
+let ps = new PS(cn.getConnectionString());
+
+ps.addChannel('messanger', function(payload){
+    amqp.connect(rabbitConnectionString, function(err, conn) {
+        conn.createChannel(function(err, ch) {
+            // let router = payload.split('.');
+            // let routerArr = [];
+            // routerArr.push({emiter: router[0]});
+            // routerArr.push({receiver: router[1]});
+            // routerArr.push({entity: router[2]});
+            // routerArr.push({event: router[3]});
+            // routerArr.push({type: router[4]});
+            // routerArr.push({additional: (router[5]) ?  router[5] : null });
+            ch.assertExchange('rabbit.notify', 'topic', {durable: false});
+            ch.publish('rabbit.notify', "", new Buffer(JSON.stringify(payload)));
+            console.log(" [x] Messanger: ", payload);
+        });
+    });
+});
+
+
